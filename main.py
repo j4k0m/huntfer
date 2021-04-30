4e0f0412823a76b7bbef07c62977a2a93d7da238
RyouYoo <ryoushin2002@outlook.fr>
2021-04-30 01:00:49 +0000

    N/A

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import re, argparse, json
+import re, argparse, json, sys
 
 TEMPLATES_PATH =  json.load(open("./config.json", "r"))["TEMPLATES_PATH"]
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument('-t', type=str, help="Template name to use, example: php, js...", required=True)
+parser.add_argument('-r', type=str, help="Regex to use default: ", default="")
+
+args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+
+print(args)
